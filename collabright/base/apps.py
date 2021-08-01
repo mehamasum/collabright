edc8ef51fbc391c0291b2fb680656e72c7335902
Tanvir Hasan <tanvir002700@gmail.com>
2021-08-02 00:09:03 +0600

    add document to docuSign envelope from Document Signal

--- collabright/base/apps.py
+++ collabright/base/apps.py
@@ -7,8 +7,11 @@ class BaseConfig(AppConfig):
     name = 'collabright.base'
 
     def ready(self):
-        from .models import (Audit, )
-        from .signals import (create_docusign_envelope, )
+        from .models import (Audit, Document,)
+        from .signals import (create_docusign_envelope,
+                              add_docusign_document_to_envelope,)
 
         post_save.connect(create_docusign_envelope, Audit,
                           dispatch_uid='create_docusign_envelope')
+        post_save.connect(add_docusign_document_to_envelope, Document,
+                          dispatch_uid='add_docusign_document_to_envelope')
--- collabright/base/signals.py
+++ collabright/base/signals.py
@@ -14,3 +14,20 @@ def create_docusign_envelope(sender, instance, created, **kwargs):
         });
         envelope_id = envelope.get('envelope_id', None)
         Audit.objects.filter(id=instance.id).update(envelope_id=envelope_id)
+
+
+def add_docusign_document_to_envelope(sender, instance, created, **kwargs):
+    if not created and bool(instance.file):
+        user = instance.audit.user
+        envelope_id = str(instance.audit.envelope_id)
+        token = DocuSignOAuthService.get_access_token(user)
+        file_path = instance.file.path
+        document = DocuSignService.update_document({
+            'access_token': token,
+            'envelope_id': envelope_id,
+            'documents': [{
+                'file_path': file_path,
+                'document_id': 1
+            }]
+        })
+        print(document)
