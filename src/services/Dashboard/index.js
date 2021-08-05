4978f4870737995a00492ceceb0fe3917a822738
mehamasum <mehamasum@gmail.com>
2021-08-06 03:09:27 +0600

    Add latest audits on dashboard

--- src/services/Dashboard/index.js
+++ src/services/Dashboard/index.js
@@ -1,15 +1,19 @@
 import './index.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Card, Typography } from 'antd';
 import Banner from "./Banner";
+import AuditList from "./AuditList";
 
-const {Text} = Typography;
+const { Text } = Typography;
 
-const Dashboard = ({...props}) => {
+const Dashboard = ({ ...props }) => {
   return (
     <div>
       <Card title="Dashboard">
-        <Banner/>
+        <Banner />
+        <div className="dash-content">
+          <AuditList />
+        </div>
       </Card>
     </div>
   )
