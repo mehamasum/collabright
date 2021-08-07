cfffa7e43a34b5c55d80918c86dd5757cb5bd5e1
mehedi.masum <mehedi.masum@newscred.com>
2021-08-07 22:25:56 +0600

    Add audit metadata template

--- collabright/base/signals.py
+++ collabright/base/signals.py
@@ -4,6 +4,8 @@ from .models import Audit, Document
 from .tasks import download_and_save_file
 from .utils import compute_hash
 from django.conf import settings
+from django.template.loader import render_to_string
+
 
 # on audit
 def create_docusign_envelope(sender, instance, created, **kwargs):
@@ -12,16 +14,28 @@ def create_docusign_envelope(sender, instance, created, **kwargs):
         org_name = user.organization.name or 'Collabright Platform'
         token = DocuSignOAuthService.get_access_token(user)
         hash = compute_hash(settings.DOCUSIGN_SECRET_KEY.encode('utf-8'), str(instance.id).encode('utf-8'))
+        audit_metadata_as_document = render_to_string('audit_metadata.html', {
+            'audit_id': str(instance.id),
+            'audit_created_at': str(instance.created_at),
+            'audit_title': instance.title,
+            'audit_description': instance.description or 'N/A',
+            'audit_user': str(instance.user.id),
+            'audit_user_username': instance.user.username,
+            'audit_org': str(instance.user.organization.id) if instance.user.organization else "Personal Account",
+            'audit_orgname': instance.user.organization.name if instance.user.organization else "N/A",
+            'audit_map_url': instance.map_url,
+        })
+
         envelope = DocuSignService.create_envelope({
             'email_subject': 'Please sign the agreement from {0}'.format(org_name),
             'access_token': token,
             'signers': [],
             'documents': [{
                 'html_definition': {
-                    'source': "<html><body><div>Example HTML page source</div></body></html>"
+                    'source': audit_metadata_as_document
                 },
                 'document_id': 2,
-                'name': 'Audit Details'
+                'name': 'Audit Metadata from Collabright'
             }],
             'audit_id': instance.id,
             'audit_token': hash
