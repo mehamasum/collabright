f5a266409114dd1a3eb2eae0e4d69230974734bf
mehedi.masum <mehedi.masum@newscred.com>
2021-08-08 18:02:02 +0600

    Add back Esri/Viewer as submodule

--- scripts/copy-webviewer-files.js
+++ scripts/copy-webviewer-files.js
@@ -4,6 +4,9 @@ const copyFiles = async () => {
   try {
     await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer/webviewer-lib');
     console.log('WebViewer files copied over successfully');
+
+    await fs.copy('./Viewer/build', './public/embeds/esriviewer-lib');
+    console.log('Esri Viewer files copied over successfully');
   } catch (err) {
     console.error(err);
   }
