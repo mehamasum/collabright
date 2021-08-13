0d1f3cfc88e0d7ddda3a4b4420b3ccea0996ef15
mehamasum <mehamasum@gmail.com>
2021-08-13 18:57:05 +0600

    Handle api excepttion while creating envelope

--- collabright/base/signals.py
+++ collabright/base/signals.py
@@ -5,13 +5,15 @@ from .tasks import download_and_save_file
 from .utils import compute_hash
 from django.conf import settings
 from django.template.loader import render_to_string
-
+from docusign_esign.client import api_exception
+import logging
+logger = logging.getLogger(__name__)
 
 # on audit
 def create_docusign_envelope(sender, instance, created, **kwargs):
     if created:
         user = instance.user
-        org_name = user.organization.name or 'Collabright Platform'
+        org_name = user.organization.name or user.username
         token = DocuSignOAuthService.get_access_token(user)
         hash = compute_hash(settings.DOCUSIGN_SECRET_KEY.encode('utf-8'), str(instance.id).encode('utf-8'))
         audit_metadata_as_document = render_to_string('audit_metadata.html', {
@@ -26,22 +28,26 @@ def create_docusign_envelope(sender, instance, created, **kwargs):
             'audit_map_url': instance.map_url,
         })
 
-        envelope = DocuSignService.create_envelope({
-            'email_subject': 'Please sign the agreement from {0}'.format(org_name),
-            'access_token': token,
-            'signers': [],
-            'documents': [{
-                'html_definition': {
-                    'source': audit_metadata_as_document
-                },
-                'document_id': 2,
-                'name': 'Audit Metadata from Collabright'
-            }],
-            'audit_id': instance.id,
-            'audit_token': hash
-        })
-        envelope_id = envelope.get('envelope_id', None)
-        Audit.objects.filter(id=instance.id).update(envelope_id=envelope_id)
+        try:
+            envelope = DocuSignService.create_envelope({
+                'email_subject': 'Please sign the agreement from {0} - Powered by Collabright'.format(org_name),
+                'access_token': token,
+                'signers': [],
+                'documents': [{
+                    'html_definition': {
+                        'source': audit_metadata_as_document
+                    },
+                    'document_id': 2,
+                    'name': 'Audit Metadata from Collabright'
+                }],
+                'audit_id': instance.id,
+                'audit_token': hash
+            })
+            envelope_id = envelope.get('envelope_id', None)
+            Audit.objects.filter(id=instance.id).update(envelope_id=envelope_id)
+        except api_exception.ApiException as e:
+            logger.error('Failed to create docusign envelope for audit {0}'.format(str(instance.id)))
+        
 
 def export_map_as_pdf(sender, instance, created, **kwargs):
     document = instance
